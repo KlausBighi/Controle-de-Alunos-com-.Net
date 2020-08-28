bbf02b69a7f205b83ea4ac2394adbb8a218e5a31
Klaus Vitor Marques Bighi <56765311+KlausBighi@users.noreply.github.com>
2020-08-28 17:38:02 -0300

    Estrutura de repeticao criada

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a Opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar Alunos");
+            Console.WriteLine("3- Calcular Media Geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "x");
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                    //TODO: Adicionar Aluno
+                        break;
+                    case "2":
+                    //TODO: listar Alunos
+                        break;
+                    case "3":
+                    //TODO: Calcular Media Total
+                        break;
+                        
+                        default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            Console.WriteLine("Informe a Opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar Alunos");
+            Console.WriteLine("3- Calcular Media Geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+        }
+    }
+}
